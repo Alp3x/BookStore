508d7b05ec41534992479dbbdbe3c30adf807f01
Francisco <fran7carvalhogmail.com>
2024-12-01 01:10:53 +0000

    FinalStintOfTheNigth Vid=>4:23:00

--- BookApp/Controllers/CategoryController.cs
+++ BookApp/Controllers/CategoryController.cs
@@ -22,23 +22,82 @@ namespace BookApp.Controllers
             return View();
         }
         [HttpPost]
-		public IActionResult Create(Category obj)
-		{
-   //         if(obj.Name == obj.DisplayOrder.ToString())
-   //         {
-   //             ModelState.AddModelError("", "The Display order cant match the name");
-   //         }
-			//if (obj.Name != null && (obj.Name == "test" || obj.Name == "Teste"))
-			//{
-			//	ModelState.AddModelError("","test is an Invalid Name");
-			//}
-			if (ModelState.IsValid)
+        public IActionResult Create(Category obj)
+        {
+            //if(obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //  ModelState.AddModelError("", "The Display order cant match the name");
+            //}
+            //if (obj.Name != null && (obj.Name == "test" || obj.Name == "Teste"))
+            //{
+            //	ModelState.AddModelError("","test is an Invalid Name");
+            //}
+            if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+				TempData["success"] = "Category created sucessfully";
                 return RedirectToAction("Index");
             }
             return View();
+        }
+		public IActionResult Edit(int id)
+		{
+            if( id == 0)
+            {
+                return NotFound();
+            }
+            // 3 Formas de Tirar um valor da Base de Dados
+            Category? categoryFromDb = _db.Categories.Find(id);
+			//Category? categoryFromDb = _db.Categories.FirstOrDefault(u=>u.Id==id);
+			//Category? categoryFromDb = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+			if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+			return View(categoryFromDb);
+		}
+
+		[HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Update(obj);
+				_db.SaveChanges();
+				TempData["success"] = "Category updated sucessfully";
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+		public IActionResult Delete(int id)
+		{
+			if (id == 0)
+			{
+				return NotFound();
+			}
+			// 3 Formas de Tirar um valor da Base de Dados
+			Category? categoryFromDb = _db.Categories.Find(id);
+			//Category? categoryFromDb = _db.Categories.FirstOrDefault(u=>u.Id==id);
+			//Category? categoryFromDb = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+		}
+
+		[HttpPost,ActionName("Delete")]
+		public IActionResult DeletePost(int? id)
+		{
+			Category? obj = _db.Categories.Find(id);
+			if (obj == null)
+			{
+				return NotFound();
+			}
+			_db.Categories.Remove(obj);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
 		}
 	}
 }
